60e6e005c0d5d626fa0ce9d328f9654b4840a5fa
Axel Froborg <axel.froborg@gmail.com>
2020-10-28 00:35:26 +0100

    fix page starting at 0 instead of 1

--- src/main.py
+++ src/main.py
@@ -12,29 +12,33 @@ def main():
 
 
 def fetch_alcohols():
-    page = 0
+    page = 1
     alcohols = list()
     api_key = '874f1ddde97d43f79d8a1b161a77ad31'
 
     headers = {'ocp-apim-subscription-key': api_key}
 
-    while page >= 0:
-        print('Fetching alcohols from page {}'.format(page))
-        r = requests.get(
-            'https://api-extern.systembolaget.se/sb-api-ecommerce/v1/productsearch/search', headers=headers)
+    while page > 0:
+        try:
+            print('Fetching alcohols from page {}'.format(page))
+            r = requests.get(
+                'https://api-extern.systembolaget.se/sb-api-ecommerce/v1/productsearch/search?page=%a' % page, headers=headers)
 
-        if r.status_code == 200:
-            data = r.json()
+            if r.status_code == 200:
+                data = r.json()
 
-            if len(data['products']) > 0:
-                alcohols.extend(data['products'])
-                page += 1
+                if len(data['products']) > 0:
+                    alcohols.extend(data['products'])
+                    page += 1
+                else:
+                    page = -1
+
             else:
+                print('Request failed with error {}: {}'.format(
+                    r.status_code, r.json()))
                 page = -1
-
-        else:
-            print('Request failed with error {}: {}'.format(
-                r.status_code, r.json()))
+        except:
+            print('Request failed')
             page = -1
 
     mongo_service.insert_documents(alcohols)
