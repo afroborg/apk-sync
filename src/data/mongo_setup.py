24bb7b16313a0f1aec3227bad09be13b2550d99c
Axel Froborg <axel.froborg@gmail.com>
2020-10-28 11:22:31 +0100

    moved secrets to .env and added docker

--- src/data/mongo_setup.py
+++ src/data/mongo_setup.py
@@ -1,9 +1,11 @@
+import os
+
 import mongoengine
 
 
 def global_init():
     alias = 'APK'
-    db = 'localhost'
-    name = 'apk'
+    db = os.getenv('MONGO_DB_DATABASE_HOST')
+    name = os.getenv('MONGO_DB_DATABASE_NAME')
 
     mongoengine.register_connection(alias, db, name)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
+import os
+
 import requests
+from dotenv import load_dotenv
 
 import data.mongo_service as mongo_service
 import data.mongo_setup as mongo_setup
 
 
 def main():
+    load_dotenv()
     print("Running program")
     mongo_setup.global_init()
 
@@ -14,15 +18,15 @@ def main():
 def fetch_alcohols():
     page = 1
     alcohols = list()
-    api_key = '874f1ddde97d43f79d8a1b161a77ad31'
-
+    api_base_url = os.getenv('API_BASE_URL')
+    api_key = os.getenv('API_KEY')
     headers = {'ocp-apim-subscription-key': api_key}
 
     while page > 0:
         try:
             print('Fetching alcohols from page {}'.format(page))
-            r = requests.get(
-                'https://api-extern.systembolaget.se/sb-api-ecommerce/v1/productsearch/search?page=%a' % page, headers=headers)
+            r = requests.get(api_base_url +
+                             'productsearch/search?page=%a' % page, headers=headers)
 
             if r.status_code == 200:
                 data = r.json()
