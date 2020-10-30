97cdb9fd64c69fa8148ee84cb3725fcedef17621
Axel Froborg <axel.froborg@gmail.com>
2020-10-30 16:40:12 +0100

    added authentication to mongodb

--- src/data/mongo_setup.py
+++ src/data/mongo_setup.py
@@ -6,6 +6,11 @@ import mongoengine
 def global_init():
     alias = 'APK'
     db = os.getenv('MONGO_DB_DATABASE_HOST')
+    port = os.getenv('MONGO_DB_PORT')
     name = os.getenv('MONGO_DB_DATABASE_NAME')
+    username = os.getenv('MONGO_DB_USERNAME')
+    password = os.getenv('MONGO_DB_PASSWORD')
+    authentication_db = 'admin'
 
-    mongoengine.register_connection(alias, db, name)
+    mongoengine.register_connection(
+        alias=alias, db=db, port=int(port), name=name, username=username, password=password, authentication_source=authentication_db)
